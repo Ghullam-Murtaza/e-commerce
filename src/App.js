f93c351e25508b86a4fbe4b0593aa7739f6434cc
Ghullam-Murtaza <ghullammurtaza1999@gmail.com>
2023-05-28 17:14:18 +0500

    created react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        new content
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
