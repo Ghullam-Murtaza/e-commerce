23e783b55f4357bda3be126408e3fe5f16cf88fd
Ghullam-Murtaza <ghullammurtaza1999@gmail.com>
2023-06-05 22:20:22 +0500

    new update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="">
       <header className="">
-        new content added in react app
+        new content added in react app today
         
       </header>
     </div>
