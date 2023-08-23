4d6a3f15371efbda8e41d25d292f5a6ed2bfd458
Ghullam-Murtaza <ghullammurtaza1999@gmail.com>
2023-08-23 22:28:20 +0500

    added Axil Bar Component

---
+++ src/components/AxilBar/AxilBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import style from './axilBar.module.scss'
+import {Container, Row, Col} from 'react-bootstrap'
+
+const AxilBar = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <ul className="row">
+          <li>English</li>
+          <li>USD</li>
+          <li>Help</li>
+          <li>Join Us</li>
+          <li>Sign In</li>
+        </ul>
+      </div>
+      <Container className='bg-primary' fluid='md'>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
+    </ React.Fragment>
+  )
+}
+export default AxilBar
