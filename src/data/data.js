35bdd63605143ea87c9d6069d8d919a0ef9213b7
gmg <130633368+Ghullam-Murtaza@users.noreply.github.com>
2023-09-14 18:10:08 +0000

    added Main Nav or Middle Links in MainMenu.jsx

--- src/data/data.js
+++ src/data/data.js
@@ -7,3 +7,16 @@ export const axilDropdownMenuData = [
                                         ["English", "Arabic", "Spanish"],
                                         ["USD", "AUD", "EUR"]
                                     ];
+// MainMenu's Main Nav
+export const mainNavData = ["Home", "Shop", "Pages", "About", "Blog", "Contact"];
+// MainMenu's Main Nav Links
+export const mainNavSubMenuData = {
+    "Home":  ["Electronics", "NFT", "Fashion", "Jewellery", "Furniture", "Multipurpose", "RTL Version"],
+    "Shop":  ["Shop With Sidebar", "Shop no Sidebar", "Product Variation 1", "Product Variation 2", "Product Variation 3", "Product Variation 4", 
+              "Product Variation 5", "Product Variation 6", "Product Variation 7", "Product Variation 8"],
+    "Pages":  ["Wishlist", "Cart", "Checkout", "Account", "Sign Up", "Sign In", "Forgot Password", "Reset Password",
+               "Privacy Policy", "Coming Soon", "404 Error", "Typography"],
+    "Home":  ["Blog List", "Blog Grid", "Standard Post", "Gallery Post", "Video Post", "Audio Post", "Quote Post"]
+};
+
+                                    
