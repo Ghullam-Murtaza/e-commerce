a43f6fffce99821d35e6306d82f51c6e30245b42
gmg <130633368+Ghullam-Murtaza@users.noreply.github.com>
2023-09-14 18:16:34 +0000

    Update MainMenu.jsx

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -54,6 +54,7 @@ const MainMenu = () => {
                           </Nav>
                         ))}
 
+                        
                       ))}
                     </Nav.Item>
                   
